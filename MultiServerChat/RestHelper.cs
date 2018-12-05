e98050f7349862fb5dfadc5ce7963c34d0ac6239
Xekep <Xekep@users.noreply.github.com>
2018-12-05 07:02:43 +0500

    Update RestHelper.cs

--- MultiServerChat/RestHelper.cs
+++ MultiServerChat/RestHelper.cs
@@ -66,9 +66,9 @@ namespace MultiServerChat
                 string text;
 
                 if (TShock.Config.EnableGeoIP && TShock.Geo != null)
-                    text = String.Format(MultiServerChat.Config.GeoJoinFormat, TShock.Config.ServerName, ply.Name);
+                    text = String.Format(MultiServerChat.Config.GeoJoinFormat, TShock.Config.ServerName, ply.Name, ply.Country);
                 else
-                    text = string.Format(MultiServerChat.Config.JoinFormat, TShock.Config.ServerName, ply.Name, ply.Country);
+                    text = string.Format(MultiServerChat.Config.JoinFormat, TShock.Config.ServerName, ply.Name);
 
                 var message = new Message()
                 {
